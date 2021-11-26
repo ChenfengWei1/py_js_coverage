1f76f1e894be00406e37332d3d08a721fe04dc61
hejueyun <hejueyun@gmail.com>
2021-11-26 23:50:11 +0000

    py and js test tool

---
+++ js/sortTable.js
@@ -0,0 +1,34 @@
+require('jsdom-global')();
+require( 'datatables.net-buttons' );
+require( 'datatables.net-responsive' );
+require('datatables.net-responsive-bs4');
+
+var $ = require( "jquery" );
+var DataTable = require( 'datatables.net' )( window, $ );
+
+$.fn.dataTable = DataTable;
+$.fn.dataTableSettings = DataTable.settings;
+$.fn.dataTableExt = DataTable.ext;
+DataTable.$ = $;
+ 
+$.fn.DataTable = function ( opts ) {
+    return $(this).dataTable( opts ).api();
+};
+
+$(document).ready(function () {
+    $.fn.dataTable.defaults.aaSorting = [[0, 'desc']];
+    $.fn.dataTable.defaults.column.asSorting = ['desc', 'asc'];
+    $.fn.dataTable.defaults.bPaginate = false;
+    $.fn.dataTable.defaults.bFilter = false;
+    $.fn.dataTable.defaults.bInfo = false;
+    $.fn.dataTable.defaults.bSortCellsTop = true;
+    $('table.table').DataTable({
+
+        // "paging": false,
+        // "searching": false,
+        // "info": false,
+        // "retrieve": true,
+        "order": []
+    });
+    $('.dataTables_length').addClass('bs-select');
+});
---
+++ setup.sh
@@ -0,0 +1,15 @@
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update
+sudo apt-get install yarn -y
+sudo yarn add jquery
+sudo yarn add jsdom jsdom-global
+sudo yarn add datatables
+sudo yarn add datatables.net-responsive
+sudo yarn add datatables.net-buttons
+sudo yarn add datatables.net-responsive-bs4
+sudo yarn test
+pip3 install pytest
+pytest -sv --cov=test --cov=js --cov-report html
+cd htmlcov
+echo "Run index.html. e.g. google-chrome index.html"
