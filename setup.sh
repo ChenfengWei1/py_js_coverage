58e4c5fcd8829aabc8f20bd07cf3a901d5fbb3c7
hejueyun <hejueyun@gmail.com>
2021-11-28 19:15:59 +0000

    Bug Fixed

--- setup.sh
+++ setup.sh
@@ -2,14 +2,14 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update
 sudo apt-get install yarn -y
-sudo yarn add jquery
-sudo yarn add jsdom jsdom-global
-sudo yarn add datatables
-sudo yarn add datatables.net-responsive
-sudo yarn add datatables.net-buttons
-sudo yarn add datatables.net-responsive-bs4
-sudo pip3 install pytest
-sudo pip3 install pytest-cov
+yarn add jquery
+yarn add jsdom jsdom-global
+yarn add datatables
+yarn add datatables.net-responsive
+yarn add datatables.net-buttons
+yarn add datatables.net-responsive-bs4
+pip3 install pytest
+pip3 install pytest-cov
 echo "Run command: yarn test , to generate coverage report for js file."
-echo "Run command like: pytest -sv --cov=py --cov=js --cov-report html."
+echo "Run command like: python -m pytest --cov-report=term-missing --cov=src test/ --cov=js --cov-report html"
 echo "Run htmlcov/index.html."
